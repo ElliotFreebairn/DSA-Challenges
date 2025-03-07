8762fd3ab38f759391531302d8cf5019eecc6d1f
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-03-07 00:31:25 +0000

    Switch merge sort methods to static and making it prettier

--- Week2/MergeSort.java
+++ Week2/MergeSort.java
@@ -2,43 +2,33 @@
 public class MergeSort {
   public static void main(String[] args) {
     int[] unsorted = {5, 10, 1 , 7, 100, 50, 23 ,32};
-    MergeSort sorting = new MergeSort();
-    sorting.mergeSort(unsorted, 0, unsorted.length - 1);
+    MergeSort.sort(unsorted, 0, unsorted.length - 1);
     for(int i = 0; i < unsorted.length; i++) {
       System.out.println(unsorted[i]);
     }
   }
 
-  public void mergeSort(int[] a, int l, int r) {
+  public static void sort(int[] a, int l, int r) {
     if(l < r) {
       int middle = (l + r) / 2;
-      System.out.println("left: " + l + ",right: " + r + ",middle: " + middle);
-      mergeSort(a, l, middle);
-      mergeSort(a, middle + 1, r);
+      sort(a, l, middle);
+      sort(a, middle + 1, r);
       merge(a, l, middle, r);
     }
   }
 
-  public void merge(int[] a, int l, int m, int r) {
+  public static void merge(int[] a, int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
-
-    System.out.println("n1 " + n1 + ", n2 " + n2);
 
     int[] leftA = new int[n1 + 1];
     int[] rightA = new int[n2 + 1];
 
-    System.out.println("left a size: " + leftA.length + " right a size: " + rightA.length);
-
-    for(int i = 0; i < n1; i++) {
+    for(int i = 0; i < n1; i++)
       leftA[i] = a[l + i];
-      System.out.println(leftA[i] + ":");
-    }
-
-    for(int j = 0; j < n2; j++) {
+  
+    for(int j = 0; j < n2; j++)
       rightA[j] = a[m + 1 + j];
-      System.out.println(rightA + ":");
-    }
 
     leftA[n1] = Integer.MAX_VALUE;
     rightA[n2] = Integer.MAX_VALUE;
