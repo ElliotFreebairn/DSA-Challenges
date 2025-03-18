76d81e80ed5169afccefcac0ddde496ab97810d2
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-03-18 21:27:13 +0000

    Working radix sort for int array

--- LinearSorting/RadixSort.java
+++ LinearSorting/RadixSort.java
@@ -2,31 +2,61 @@
 public class RadixSort {
 
   public static void main(String[] args) {
-    int[] nums = {100, 23, 102, 40, 11};
+    int[] nums = {100, 23, 102, 1, 20, 56, 40, 11};
     int testNum = 301;
-    System.out.println(RadixSort.getDigit(testNum, 2));
     RadixSort.radixSort(nums);
-    System.out.println(Integer.valueOf("001"));
   }
 
   public static void radixSort(int[] arr) {
-    // Find largest element - digits = iterations
     int largestNum = getLargestNum(arr);
     int digitLength = String.valueOf(largestNum).length();
-    // Sort elements based on the unit place digits
-    
-    
+    int[] outputArr = new int[arr.length];
 
+    for(int i = 0; i < digitLength; i++) {
+      countingSort(arr, outputArr, i);
+
+      System.arraycopy(outputArr, 0, arr, 0, arr.length);
+      displayArr(outputArr);
+    }
   }
 
   private static int getDigit(int num, int digitX) {
-    String numAsString = String.valueOf(num);
-    final int numLength = numAsString.length() - digitX - 1;
-    if(numLength < 0) {
-      return 0;
+    return (num / (int)Math.pow(10, digitX)) % 10;
+  }
+
+  private static int getLargestNumAtDigit(int[] arr, int exp) {
+    int largestNum = 0;
+    for(int i = 0; i < arr.length; i++) {
+      if(getDigit(arr[i], exp) > largestNum) {
+        largestNum = getDigit(arr[i], exp);
+      }
     }
-    char numAsChar = numAsString.charAt(numLength);
-    return (Integer.valueOf(String.valueOf(numAsChar)));
+    return largestNum;
+  }
+
+  private static void displayArr(int[] inputArr) {
+    for(int num : inputArr) {
+      System.out.print(num + "   ");
+    }
+    System.out.println();
+  }
+
+  public static void countingSort(int[] inputArr, int[] outputArr, int exp) {
+    int[] countArr = new int[10]; // digits 0 - 9
+
+    for(int j = 0; j < inputArr.length; j++) { 
+      countArr[getDigit(inputArr[j], exp)]++;
+    }
+    
+    for(int i = 1; i < 10; i++) {
+      countArr[i] += countArr[i - 1];
+    }
+    
+    for(int j = inputArr.length - 1; j >= 0; j--) {
+      int digit = getDigit(inputArr[j], exp);
+      outputArr[countArr[digit] - 1] = inputArr[j];
+      countArr[digit]--;
+    }
   }
 
   private static int getLargestNum(int[] arr) {
@@ -39,72 +69,49 @@ public class RadixSort {
     return largestNum;
   }
 
-  private static int[] truncatedArr(int[] inputArr, int exp) {
-    int[] outputArr = new int[inputArr.length];
-    for(int i = 0; i < inputArr.length; i++) {
-      outputArr[i] = getDigit(inputArr[i], exp);
-    }
-    return outputArr;
-  }
+  //private static int[] truncatedArr(int[] inputArr, int exp) {
+  //  int[] outputArr = new int[inputArr.length];
+  //  for(int i = 0; i < inputArr.length; i++) {
+  //    outputArr[i] = getDigit(inputArr[i], exp);
+  //  }
+  //  return outputArr;
+  //}
 
-  private static int[] getEqualDigitLength(int[] inputArr, int maxLength) {
-    int[] outputArr = new int[inputArr.length];
+  //private static int[] getEqualDigitLength(int[] inputArr, int maxLength) {
+  //  int[] outputArr = new int[inputArr.length];
 
-    for(int i = 0; i < inputArr.length; i++) {
-      if(getNumLength(inputArr[i]) < maxLength) {
-        String numAsString = String.valueOf(inputArr[i]);
-        numAsString = addZeros(numAsString, maxLength - numAsString.length());
-        outputArr[i] = Integer.valueOf(numAsString);
-      } else {
-        outputArr[i] = inputArr[i];
-      }
-    }
+  //  for(int i = 0; i < inputArr.length; i++) {
+  //    if(getNumLength(inputArr[i]) < maxLength) {
+  //      String numAsString = String.valueOf(inputArr[i]);
+  //      numAsString = addZeros(numAsString, maxLength - numAsString.length());
+  //      outputArr[i] = Integer.valueOf(numAsString);
+  //    } else {
+  //      outputArr[i] = inputArr[i];
+  //    }
+  //  }
 
-    return outputArr;
-  }
+  //  return outputArr;
+  //}
   
-  private static void displayArr(int[] inputArr) {
-    for(int num : inputArr) {
-      System.out.print(num + "   ");
-    }
-    System.out.println();
-  }
+  //private static int getNumLength(int num) {
+  //  return String.valueOf(num).length();
+  //}
 
-  private static int getNumLength(int num) {
-    return String.valueOf(num).length();
-  }
+  //private static String addZeros(String num, int count) {
+  //  String zerosAndNum = null;
+  //  for(int i = 0; i < count; i++) {
+  //    zerosAndNum += "0";
+  //  }
+  //  return zerosAndNum + num;
+  //}
 
-  private static String addZeros(String num, int count) {
-    String zerosAndNum = null;
-    for(int i = 0; i < count; i++) {
-      zerosAndNum += "0";
-    }
-    return zerosAndNum + num;
-  }
-
-  private static String[] getArrAsStr(int[] arr) {
-    String[] outputArr = new String[arr.length];
-    for(int i = 0; i < arr.length; i++) {
-      outputArr[i] = String.valueOf(arr[i]);
-    }
-    return outputArr;
-  }
+  //private static String[] getArrAsStr(int[] arr) {
+  //  String[] outputArr = new String[arr.length];
+  //  for(int i = 0; i < arr.length; i++) {
+  //    outputArr[i] = String.valueOf(arr[i]);
+  //  }
+  //  return outputArr;
+  //}
 
   // Counting sort especially for radix sort
-  public static void countingSort(String[] inputArr, int[] outputArr, int k, int exp) {
-    int[] countArr = new int[k + 1];  
-
-    for(int j = 0; j < inputArr.length; j++) {
-      countArr[inputArr[j]] = countArr[getDigit(Intger.valueOf(inputArr[j]), exp)] + 1;
-    }
-    
-    for(int i = 1; i <= k; i++) {
-      countArr[i] = countArr[i] + countArr[i - 1];
-    }
-    
-    for(int j = inputArr.length - 1; j >= 0; j--) {
-      outputArr[countArr[inputArr[j]] - 1] = inputArr[j];
-      countArr[inputArr[j]] -= 1;
-    }
-  }
-}
+ }
