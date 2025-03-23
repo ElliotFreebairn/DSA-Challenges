0764ee9c51b6643c186d2dba4d0d7cbdd8aa08b6
Elliot Freebairn <elliotfreebairn1@gmail.com>
2025-03-23 21:45:20 +0000

    Added a left rotate function for the tree, working on implementing a right rotate function

--- Trees/RedBlackTree.java
+++ Trees/RedBlackTree.java
@@ -1,6 +1,33 @@
+class RedBlackTree {
+  Node root;
 
+  private static void leftRotate(Node x) {
+    Node y = n.right;
+    
+    n.right = y.left;
+    if(y.left != null) {
+      y.left.parent = x;
+    }
+    y.parent = x.parent;
 
+    if(x.parent == null) {
+      root = y;
+    }else if(x == x.p.left) {
+      x.parent.left = y;
+    } else {
+      x.p.right = y;
+    }
 
+    y.left = x;
+    x.p = y;
+  }
+
+  private static void rightRotate(Node y) {
+    Node x = n.left;
+
+    y.left = x.right;
+  }
+}
 
 class Node {
   char colour;
@@ -9,5 +36,9 @@ class Node {
   Node right;
   Node parent;
 
-  public Node()
+  public Node(int key) {
+    this.key = key;
+  }
 }
+
+
